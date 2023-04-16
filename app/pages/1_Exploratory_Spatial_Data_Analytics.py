8a12c9a719189f90e76d8cb62d8d27d183da5316
jenhar <jenhar.hidayat@gmail.com>
2023-04-17 06:18:53 +0700

    css EDA

--- app/pages/1_Exploratory_Spatial_Data_Analytics.py
+++ app/pages/1_Exploratory_Spatial_Data_Analytics.py
@@ -307,7 +307,7 @@ with tab5:
                     use_container_width=True)
 
 # Style
-with open('style.css')as f:
+with open(Path(__file__).parents[1]/'style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 make_map_responsive = """
